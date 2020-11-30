6332dba8c825b7ca5197836dd868be6b93903335
Vincent C. Mader <vincent.mader@icloud.com>
2020-11-30 21:26:24 +0100

    continued on ex 04

--- 04/code/n_body_sim.py
+++ 04/code/n_body_sim.py
@@ -1,3 +1,4 @@
+from random import uniform
 from tqdm import tqdm
 
 import matplotlib.pyplot as plt
@@ -6,7 +7,6 @@ import numpy as np
 
 G = 1.
 dt = 1e-3
-steps = 30000
 
 colors = ['blue', 'green', 'red']
 
@@ -26,7 +26,7 @@ class Planet():
         for p in planets:
             if p == self:
                 continue
-            epsilon = 1e-6  # TODO
+            epsilon = 2 * len(planets)**(-1/3) / 10000
             delta_r = p.r - self.r
             a = G * p.m * delta_r / (norm(delta_r)**3 + epsilon**3)
             self.v = np.add(self.v, a * dt)
@@ -56,7 +56,32 @@ def create_3_body_system():
     return [p1, p2, p3]
 
 
-def plot_system(planets):
+def create_30_body_system():
+    planets = []
+    for _ in range(30):
+        valid_r, valid_v = False, False
+        while not valid_r:
+            x = uniform(-1, 1)
+            y = uniform(-1, 1)
+            z = uniform(-1, 1)
+            if np.sqrt(x**2 + y**2 + z**2) <= 1:
+                valid_r = True
+        while not valid_v:
+            vx = uniform(-.1, .1)
+            vy = uniform(-.1, .1)
+            vz = uniform(-.1, .1)
+            if np.sqrt(vx**2 + vy**2 + vz**2) <= .1:
+                valid_v = True
+
+        r, v, m = np.array([x, y, z]), np.array([vx, vy, vz]), 1
+        p = Planet(r, v, m)
+
+        planets.append(p)
+
+    return planets
+
+
+def plot_system(planets, steps):
     for _ in tqdm(range(steps)):
         for p in planets:
             p.update_velocity(planets)
@@ -70,6 +95,10 @@ def plot_system(planets):
             plt.xlabel(r'$y$ coordinate')
             plt.ylabel(r'$x$ coordinate')
             plt.plot(y, x, color=colors[idx])
+        elif len(planets) == 30:
+            plt.xlabel(r'$y$ coordinate')
+            plt.ylabel(r'$x$ coordinate')
+            plt.plot(y, x, color='black')
         else:
             plt.xlabel(r'$x$ coordinate')
             plt.ylabel(r'$y$ coordinate')
@@ -83,13 +112,18 @@ def main():
 
     plt.figure(figsize=(8, 4))
     planets = create_2_body_system()
-    plot_system(planets)
+    plot_system(planets, steps=30000)
     plt.close()
 
     plt.figure(figsize=(8, 4))
     planets = create_3_body_system()
-    plot_system(planets)
+    plot_system(planets, steps=30000)
     plt.close()
+
+    # plt.figure(figsize=(8, 4))
+    # planets = create_30_body_system()
+    # plot_system(planets, steps=1000)
+    # plt.close()
 
 
 if __name__ == "__main__":
