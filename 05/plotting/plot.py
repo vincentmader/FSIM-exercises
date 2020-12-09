3d931f663ab390a2593da81d9a328011a527fc9f
Vincent C. Mader <vincent.mader@icloud.com>
2020-12-09 19:37:54 +0100

    continued on exercise 5

--- 05/plotting/plot.py
+++ 05/plotting/plot.py
@@ -6,24 +6,35 @@ import numpy as np
 data = np.loadtxt('../MolDyn/output_T80.txt', skiprows=1)
 
 time = [i[0] for i in data]
-# E1 = [i[1] for i in data]
-E2 = [i[2] for i in data]
-E3 = [i[3] for i in data]
-E4 = [i[4] for i in data]
+E_kin = np.array([i[2] for i in data])
+E_pot = np.array([i[3] for i in data])
+E_tot = np.array([i[4] for i in data])
+
+E_kin0, E_pot0, E_tot0 = E_kin[0], E_pot[0], E_tot[0]
 
 # plot
 plt.figure(figsize=(8, 4))
 
 # plt.plot(time, E1, label='column 1')
-plt.plot(time, E2, label=r'$E_{kin}$')
-plt.plot(time, E3, label=r'$E_{pot}$')
-plt.plot(time, E4, label=r'$E_{tot}$')
+plt.plot(time, E_kin, label=r'$E_{kin}$')
+plt.plot(time, E_pot, label=r'$E_{pot}$')
+plt.plot(time, E_tot, label=r'$E_{tot}$')
 plt.plot(time, [0] * len(time), '--', color='grey')
 
-plt.title('evolution of total energy over 20000 integration steps')
+plt.title('evolution of energies')
 plt.xlabel(r'$t/\tau$')
-plt.ylabel(r'$E/\sigma$')
+plt.ylabel(r'$E/\epsilon$')
 plt.xlim(time[0], time[-1])
 plt.legend(loc='upper left')
 
 plt.savefig('../figures/energy.pdf')
+
+plt.figure(figsize=(8, 4))
+plt.plot(time, (E_tot - E_tot0) / E_tot0, label=r'$E_{tot}$')
+plt.title('evolution of relative deviation of total energy')
+plt.xlabel(r'$t/\tau$')
+plt.ylabel(r'$\Delta E/(E_0\epsilon)$')
+plt.xlim(time[0], time[-1])
+plt.legend(loc='upper left')
+
+plt.savefig('../figures/relative_energy.pdf')
