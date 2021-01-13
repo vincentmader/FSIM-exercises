ec423d9bd700d58f94cf7954ecb6f6dd649ef84f
edeljor <j.edelmann@stud.uni-heidelberg.de>
2021-01-13 21:51:41 +0100

    aufraumen

--- 07/code/Aufg2.py
+++ 07/code/Aufg2.py
@@ -12,13 +12,16 @@ e = 1
 a, b, c, v = create_matrices(N, L*L*e/D, T0)
 M = np.diag(a) + np.diag(b, 1) + np.diag(c, -1)
 
+# c
+P = prolongation(N)
+R = restriction(N)
+print(M)
+print(P@R@M)
+
+# d
 x = np.zeros(N)
-x = Vcycle(M, v, x, 2, N,smooth=3, solve=40)
+x = Vcycle(M, v, x, 2, N, smooth=3, solve=40)
 print(x)
 
-# P = prolongation(N)
-# R = restriction(N)
-# print(M)
-# print(P@R@M)
 
 print("Done")
