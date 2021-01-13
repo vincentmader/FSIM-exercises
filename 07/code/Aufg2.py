560570e7d3347c81f0168fc7d254e8c2ff3effda
edeljor <j.edelmann@stud.uni-heidelberg.de>
2021-01-13 22:29:04 +0100

    paar bearb

--- 07/code/Aufg2.py
+++ 07/code/Aufg2.py
@@ -6,21 +6,21 @@ from v_cicle import Vcycle
 D = 0.5
 T0 = 1
 L = 1
-N = 9
+N = 65
 e = 1
 
 a, b, c, v = create_matrices(N, L*L*e/D, T0)
 M = np.diag(a) + np.diag(b, 1) + np.diag(c, -1)
 
 # c
-P = prolongation(N)
-R = restriction((N-1)/2 + 1)
+R = restriction(int(N))
+P = prolongation(int((N-1)/2+1))
 print(M)
 print(P@R@M)
 
 # d
 x = np.zeros(N)
-x = Vcycle(M, v, x, 2, N, smooth=3, solve=40)
+x = Vcycle(M, v, x, 5, N, smooth=3, solve=40)
 print(x)
 
 
