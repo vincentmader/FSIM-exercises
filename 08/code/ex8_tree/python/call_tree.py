d201666c9250e35091c887de3ebaa1adccf7ebb3
Vincent C. Mader <vincent.mader@icloud.com>
2021-01-20 20:59:54 +0100

    linted

--- 08/code/ex8_tree/python/call_tree.py
+++ 08/code/ex8_tree/python/call_tree.py
@@ -55,6 +55,8 @@ fexact = deepcopy(q.forces)
 # ... TO BE FILLED IN ...
 print "Please replace this print statement with your own code"
 '''
+
+
 def call_tree(N, angle):
     print('N = {}, angle = {} \n'.format(N, angle))
     particles = np.zeros((N, 3))
@@ -62,20 +64,18 @@ def call_tree(N, angle):
         x = random()
         y = random()
         z = random()
-        particles[i] = np.array([x,y,z])
+        particles[i] = np.array([x, y, z])
 
-    q=TreeClass(particles)
+    q = TreeClass(particles)
     q.insertallparticles()
     q.computemultipoles(0)
-
 
     print("starting tree gravity")
     t0 = time.time()
     q.allgforces(angle)
     t1 = time.time()
     treegrav_dt = t1-t0
-    print("done in "+str(treegrav_dt)+" seconds")
-
+    print("done in " + str(treegrav_dt) + " seconds")
     print("the mean number of interactions is {}\n".format(np.mean(q.interactions)))
 
     fapprox = deepcopy(q.forces)
@@ -85,29 +85,34 @@ def call_tree(N, angle):
 
     # ... TO BE FILLED IN ...
     acc = np.zeros((len(particles), 3))
-    for index, body in enumerate(particles):        #itterate over all other bodies
-        a   = 0
+    for index, body in enumerate(particles):  # itterate over all other bodies
+        a = 0
         for j in range(len(particles)):
             if index != j:
                 r = np.linalg.norm(body - particles[j])
                 tmp = 1 / r**3
                 a += tmp*(body - particles[j])
-        acc[index]  = a
+        acc[index] = a
 
     t1 = time.time()
     fullgrav_dt = t1-t0
-    print("done in "+str(fullgrav_dt)+" seconds\n")
+    print("done in " + str(fullgrav_dt) + " seconds\n")
 
-    fexact = acc    #m=1 => F = a
+    fexact = acc  # m=1 => F = a
 
     # ... TO BE FILLED IN ...
+
     rel_err = np.mean(np.abs((fapprox-fexact)/fexact))*100
-    drel    = np.std((fapprox-fexact)/fexact)*100/np.sqrt(N)
+    drel = np.std((fapprox-fexact)/fexact)*100/np.sqrt(N)
+
     print(r"the relative error is: {:.2f} \pm {:.2f}%".format(rel_err, drel))
 
 
 # for N in [5000]: # ,10000,20000,40000]: #
-
 # for angle in [0.2, 0.4,0.8]:
-call_tree(int(sys.argv[1]), float(sys.argv[2]))
+
+call_tree(
+    int(sys.argv[1]),   # particle number (integer)
+    float(sys.argv[2])  # opening angle (float between 0 and 1)
+)
 print('---------------------------------------------------\n')
