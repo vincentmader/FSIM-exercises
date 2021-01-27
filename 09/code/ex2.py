c6532157ef70ccd4ea52d8203427d55744133b1a
Vincent C. Mader <vincent.mader@icloud.com>
2021-01-28 00:56:40 +0100

    continued on ex09

--- 09/code/ex2.py
+++ 09/code/ex2.py
@@ -5,26 +5,26 @@ import matplotlib.pyplot as plt
 def advect(qold, vold, dx, dt):
     masksym = np.where(vold[:-1]*vold[1:] < 0)
     maskupwind = list(
-            set(np.where(np.sign(v[1:]) == 1)[0]) &
-            set(np.where(np.sign(v[:-1]) == 1)[0])
-            )
+        set(np.where(np.sign(v[1:]) == 1)[0]) &
+        set(np.where(np.sign(v[:-1]) == 1)[0])
+    )
     maskdownwind = list(
         set(np.where(np.sign(v[1:]) == -1)[0]) &
         set(np.where(np.sign(v[:-1]) == -1)[0])
-                     )
+    )
     # print(masksym)
     # print(maskupwind)
     # print(maskdownwind)
     v_mean = (vold[1:] + vold[:-1])/2
     # symmetric
     qold[1:-1][masksym] -= (qold[2:][masksym] - qold[:-2][masksym]) / (2*dx) * \
-                                v_mean[masksym] * dt
+        v_mean[masksym] * dt
     # upwind
     qold[1:-1][maskupwind] -= (qold[1:-1][maskupwind] - qold[:-2][maskupwind])\
-                                / dx * vold[:-1][maskupwind] * dt
+        / dx * vold[:-1][maskupwind] * dt
     # downwind
     qold[1:-1][maskdownwind] -= (qold[2:][maskdownwind] - qold[1:-1][maskdownwind]) / dx \
-                                * vold[1:][maskdownwind] * dt
+        * vold[1:][maskdownwind] * dt
     return qold
 
 
@@ -35,6 +35,7 @@ v = np.ones(x_N+1)
 x = np.linspace(-L/2-dx, L/2+dx, num=x_N+2)
 q_0 = (x - np.abs(x))/(2*x)
 dt = 3.0 / 100
+
 for i in range(x_N):
     q_0 = advect(q_0, v, dx, dt)
 plt.plot(x, q_0)
@@ -42,20 +43,20 @@ plt.savefig("../figures/plot1.png")
 
 
 v = -2/L * np.linspace(-L/2-dx/2, L/2 + dx/2, x_N+1)
-# print(x)
-# print(v)
 q_0 = np.zeros_like(x)
+
 for i in range(len(x)):
-    # print(np.abs(x[i]), L/4)
     if (np.abs(x[i]) < L/4):
         q_0[i] = 1
-# print(len(q_0))
 plt.figure()
-plt.plot(x, q_0, label="0")
+plt.plot(x, q_0, label="t=0")
+
 for i in range(x_N):
     q_0 = advect(q_0, v, dx, dt)
-plt.plot(x, q_0, label="1")
+plt.plot(x, q_0, label="t=3")
 plt.legend()
+plt.xlabel('x')
+plt.ylabel('q(x)')
 plt.savefig("../figures/plot2.png")
 
 print('Done.')
--- 09/code/task_1.py
+++ 09/code/task_1.py
@@ -16,13 +16,15 @@ def plot(q, task, scheme='upwind', ts=[0, 100], xlims=None):
 
     colors = ['green', 'red']
     for idx, t in enumerate(ts):
-        plt.plot(x, q[t], label=f"t={t}", color=colors[idx])
+        label = 3 if idx else 0
+        plt.plot(x, q[t], label=f"t={label}", color=colors[idx])
 
     plt.xlabel('x/L')
     plt.ylabel('q(x)')
     if xlims:
-        plt.xlim(xlims[0], xlims[1])
-        plt.xticks([xlims[0], 0, xlims[1]], [xlims[0]/L, 0, xlims[1]/L])
+        # plt.xlim(xlims[0], xlims[1])
+        # plt.xticks([xlims[0], 0, xlims[1]], [xlims[0]/L, 0, xlims[1]/L])
+        pass
     else:
         plt.xticks([-L/2, 0, L/2], ['$-1/2$', 0, '1/2'])
 
@@ -45,10 +47,10 @@ plot(q, 5)
 # exercise 01, part 6, 7
 xlims = [-.1*L, .1*L]
 q = advection(q_0, t_N=100)
-plot(q, '6_0', ts=[100], xlims=xlims)
+plot(q, '6_0', ts=[0, 100], xlims=xlims)
 q = advection(q_0, t_N=1000)
-plot(q, 6, ts=[1000], xlims=xlims)
+plot(q, 6, ts=[0, 1000], xlims=xlims)
 q = advection(q_0, t_N=10)
-plot(q, 7, ts=[10], xlims=xlims)
+plot(q, 7, ts=[0, 10], xlims=xlims)
 
 print("Done.")
