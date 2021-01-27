7d60bcccd7fd0568ae8bf50f85186fab8bf681c4
Vincent C. Mader <vincent.mader@icloud.com>
2021-01-27 22:14:35 +0100

    continued on ex 09

--- 09/code/task_1.py
+++ 09/code/task_1.py
@@ -13,7 +13,6 @@ q_0 = (x - np.abs(x))/(2*x)
 
 def plot(q, task, scheme='upwind', ts=[0, 250, 500]):
     plt.figure(figsize=(6, 4))
-    # print(q, ts)
 
     colors = ['green', 'yellow', 'red']
     for idx, t in enumerate(ts):
@@ -22,7 +21,7 @@ def plot(q, task, scheme='upwind', ts=[0, 250, 500]):
     plt.xticks([-L, 0, L], ['$-1$', 0, '1'])
     plt.xlabel('x/L')
     plt.ylabel('q(x)')
-    plt.xlim(-L, L)
+    # plt.xlim(-L, L)
 
     plt.legend()
     plt.savefig(f"../figures/{scheme}_{task}.png")
@@ -37,11 +36,11 @@ for idx, scheme in enumerate(['symmetric', 'upwind', 'downwind']):
 # exercise 01, part 4, 5
 q = advection(q_0, boundary_conditions=[.5, 0], t_N=10000)
 plot(q, 4, ts=[0, 1000, 10000])
-q = advection(q_0, boundary_conditions=[0, .5], t_N=10000)
+q = advection(q_0, boundary_conditions=[1, .5], t_N=10000)
 plot(q, 5, ts=[0, 1000, 10000])
 
 # exercise 01, part 6, 7
 q = advection(q_0, t_N=1000)
-plot(q, 6, ts=[0, 500, 1000])
+plot(q, 6, ts=[3])
 q = advection(q_0, t_N=10)
-plot(q, 7, ts=[0, 5, 10])
+plot(q, 7, ts=[3])
